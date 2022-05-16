c424c3690b565fda164416e268e4fd134b60bee9
denizerbay <denizerbayy@gmail.com>
2022-05-16 20:49:58 +0300

    Initial check-in of module jwt

---
+++ jwt/Controllers/DefaultController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using jwt.DAL;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace jwt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : Controller
+    {
+        [HttpGet("[action]")]
+        public IActionResult Login()
+        {
+            return Created("", new BuildToken().CreateToken());
+        }
+
+        [HttpGet("[action]")]
+        [Authorize]
+        public IActionResult TestPage()
+        {
+            return Ok("Test Başarılı");
+        }
+    }
+}
---
+++ jwt/DAL/BuildToken.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace jwt.DAL
+{
+    public class BuildToken
+    {
+        public string CreateToken()
+        {
+            var bytes = Encoding.UTF8.GetBytes("jwtTestStartupCodeBehind");
+            SymmetricSecurityKey key = new SymmetricSecurityKey(bytes);
+            SigningCredentials credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            JwtSecurityToken jwtSecurityToken = new JwtSecurityToken(issuer: "http://localhost", audience: "http://localhost", notBefore: DateTime.Now, expires:DateTime.Now.AddMinutes(10), signingCredentials:credentials);
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            return handler.WriteToken(jwtSecurityToken);
+        }
+    }
+}
